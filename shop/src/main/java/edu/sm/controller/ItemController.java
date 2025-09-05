eb77acbe9356cc496b5bf9e7b0ad02cc895807f9
tj003 <tj0032@naver.com>
2025-09-05 13:22:43 +0900

    0905

---
+++ shop/src/main/java/edu/sm/controller/ItemController.java
@@ -0,0 +1,33 @@
+package edu.sm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/item")
+public class ItemController {
+
+    String dir="item/";
+
+    @RequestMapping("")
+    public String main(Model model) {
+        model.addAttribute("center",dir+"center");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+    @RequestMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("center",dir+"add");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+    @RequestMapping("/get")
+    public String get(Model model) {
+        model.addAttribute("center",dir+"get");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+}
